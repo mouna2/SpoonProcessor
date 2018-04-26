9d6eefeca82ee6456e85dc0928612eb0c8728a42
mouna <mouna@DESKTOP-UAD8M32>
2018-04-26 21:42:28 +0200

    mycommit

---
+++ SpoonProcessor/src/main/java/Main.java
@@ -0,0 +1,70 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import spoon.Launcher;
+import spoon.SpoonAPI;
+import spoon.reflect.CtModel;
+import spoon.reflect.code.CtFieldAccess;
+import spoon.reflect.code.CtFieldRead;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtModule;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.factory.ClassFactory;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.factory.TypeFactory;
+import spoon.reflect.visitor.Filter;
+import spoon.reflect.visitor.filter.FieldAccessFilter;
+
+public class Main {
+	static List<String> classnames= new ArrayList<String>(); 
+	public static void main(String[] args) {
+		// Build model
+		
+		DBDemo dao = new DBDemo();
+	   
+	    
+		SpoonAPI spoon = new Launcher();
+    	spoon.addInputResource("C:\\Users\\mouna\\Downloads\\chessgantcode\\workspace_codeBase\\Chess");
+    	spoon.getEnvironment().setAutoImports(true);
+    	spoon.getEnvironment().setNoClasspath(true);
+    	CtModel model = spoon.buildModel();
+    	
+  
+    	// Interact with model
+    	Factory factory = spoon.getFactory();
+    	ClassFactory classFactory = factory.Class();
+    	
+    	
+    	for(CtType<?> clazz : classFactory.getAll()) {
+    		
+    		System.out.println(clazz.getSimpleName());
+    		classnames.add(clazz.getQualifiedName()); 
+    		clazz.getSuperclass();
+    		
+    		clazz.getSuperInterfaces();
+    		
+    	
+   
+    		
+    		
+    		 for(CtField<?> field : clazz.getFields()) {
+    				for(CtMethod<?> method :clazz.getMethods()) {
+    	    			// method.getParameters()
+    	    			method.<CtFieldAccess<?>>getElements(new FieldAccessFilter(field.getReference()));
+    	    		}
+    		 }
+    	}
+    	
+    	
+    	 Classnames(); 
+    	
+	}
+	
+	public static List<String> Classnames(){
+		return classnames; 
+	}
+}
